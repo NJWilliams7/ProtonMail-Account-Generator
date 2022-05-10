daf993df23d16fc91fa153613c5edb5ddda13980
NJWilliams7 <whovalP492464@gmail.com>
2022-05-09 23:11:56 -0400

    fixed captcha selection and username

--- ProtonMail_Generator.py
+++ ProtonMail_Generator.py
@@ -11,9 +11,6 @@ import secrets
 from random import randint
 from selenium.webdriver.support.wait import WebDriverWait
 import random
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
 # username generation
@@ -22,7 +19,7 @@ username = output_string
 print(username)
 # end username generation
 
-# password generation (12+ char)
+# password generation
 import string
 import secrets
 alphabet = string.ascii_letters + string.digits
@@ -36,7 +33,7 @@ print(password)
 # end password generation
 
 # create txt file with username and password
-dirname = os.path.dirname('write dirname here')
+dirname = os.path.dirname('/filepath to folder containing Python script/')
 txt_path = os.path.join(dirname, 'proton_username_pw.txt')
 txt_file = open(txt_path, "a")
 
@@ -44,22 +41,21 @@ random_int = randint(111111,999999)
 txt_file.write("username: "+ username + str(random_int) + " password: " + password)
 txt_file.write("\n")
 txt_file.close()
-username_vf = username#+str(random_int)
+username_vf = username+str(random_int)
 
 time.sleep(1)
 
 # ProtonMail Account Generation
 driver = webdriver.Chrome()
 driver.get('https://account.protonmail.com/signup?language=en')
-time.sleep(8)
+time.sleep(6)
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized");
 options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
-time.sleep(8)
+time.sleep(6)
 
 #input username
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
 actions = ActionChains(driver)
@@ -67,31 +63,23 @@ text = username_vf
 for character in text:
     actions.send_keys(character)
     actions.perform()
-    time.sleep(0.3) # pause for 0.3 seconds
+    time.sleep(0.2) # pauses for 0.2 seconds in between chars to avoid glitch
 
 #input passwords
 driver.find_element(by=By.ID, value='password').send_keys(password)
-time.sleep(1)
+time.sleep(8)
 driver.find_element(by=By.ID, value='repeat-password').send_keys(password)
-time.sleep(1)
+time.sleep(8)
 driver.find_element(by=By.XPATH, value="//button[contains(text(),'Next')]").click()
-time.sleep(10)
+time.sleep(8)
 driver.find_element(by=By.XPATH, value="//button[contains(text(),'Skip')]").click()
-time.sleep(10)
+time.sleep(8)
 driver.find_element(by=By.XPATH, value="//button[contains(text(),'Confirm')]").click()
-time.sleep(10)
+time.sleep(8)
 driver.find_element(by=By.XPATH, value="//button[contains(text(),'Select plan')]").click()
-time.sleep(10)
+time.sleep(8)
 
-
-# manually solve Captcha window #not working yet
-options = webdriver.ChromeOptions() 
-options.add_argument("start-maximized")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-
-driver.find_element_by_xpath('//button[@id="checkbox")]').click()
-
-#WebDriverWait(driver,10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[title='Captcha']"))) 
-#WebDriverWait(driver,10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[title='widget containing checkbox for hCaptcha security challenge']")))
-#WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "username"))).click()
+# manually solve Captcha window after code clicks checkbox
+WebDriverWait(driver, 5).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[@class='w100 h-custom' and @title='Captcha']")))
+WebDriverWait(driver, 5).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[@title='widget containing checkbox for hCaptcha security challenge']")))
+WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='anchor']"))).click()
